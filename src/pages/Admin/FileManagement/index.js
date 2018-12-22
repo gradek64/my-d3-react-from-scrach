4f7f34d207da02b5d0b8b2e25873c52509b9a64a
GILGRZ <gradek64@yahoo.com>
2018-12-22 15:55:07 +0000

    file-management-page file types finished

--- src/pages/Admin/FileManagement/index.js
+++ src/pages/Admin/FileManagement/index.js
@@ -1,28 +1,113 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 import SimpleTable from '../../../components/SimpleTable';
+import { withRouter } from 'react-router-dom';
 import costModelService from '../../../services/cost-model-mock';
-
+import events from '../../../utils/events';
+import Typography from '@material-ui/core/Typography';
+import ModalCustom from '../../../components/modal';
+import costpotsMockService from '../../../services/costpots-mock';
+import fileTypesMockService from '../../../services/filetypes-mock';
+import './fileManagement.scss';
 
 class CostModel extends React.Component {
 state = {
   data:null
+
 }
 async componentDidMount(){
+
+  const { costModelId, costPotId } = this.props;
+
+  const currentCostPot = await costpotsMockService.getAll(costModelId)
+    .then((res)=>res.data)
+    .then((costPots)=>costPots.find(
+      (current)=>current.id === Number(costPotId)
+    ));
+  //set costPot Name;
+  this.setState({costPotName:currentCostPot.name});
+
+  //get files for current CostPot
+  const fileTypes = await fileTypesMockService.getAll(currentCostPot.resourceTypeId)
+    .then((res) => res.data);
+    // ? scope.fileTypeItems = scope.fileTypes.map(buildFileTypeItem);
+
+  console.log('fileTypes', fileTypes);
+
+  //to do fill the table with below request
+  /* if (!scope.fileTypes) {
+    return;
+  }
+  scope.isLoading = true;
+
+  // filesService
+  filesMockService
+    .getAll(scope.fileTypes, $scope.tableState, $scope.costpot.id)
+    .then((result) => {
+      scope.items = result.data;
+      console.log('files back', result.data);
+      if (scope.items.length === 0) {
+        scope.errorOrNoData = 'No files have been uploaded';
+      }
+    })
+    .catch((err) => {
+      console.log('ERROR: ', err);
+      scope.errorOrNoData = 'There is an error. See console log.';
+    })
+    .finally(() => {
+      scope.isLoading = false;
+    });*/
+
+
+
   const dataResponsone = await costModelService.getAll();
   const data = dataResponsone.data;
   this.setState(() => ({ data }));
 }
 
-render(){
-  if(this.state.data){
+/*if(this.state.data){
     return (
       <SimpleTable data={this.state.data} />
     );
   }else {
     return '....preloading';
-  }
+  }*/
+
+render(){
+  const {costPotName, data } = this.state;
+
+  return (
+    <div className='fileManagement'>
+    
+      <div className='pageTop'>
+        <Typography component="h2" variant="h2" gutterBottom>
+                  File Management for {costPotName?costPotName:'...loading'}
+        </Typography>
+        <Button variant="contained" color="primary" onClick={()=>{events.emit('CLICK_ON_CANCEL_COST_MODEL');}}>Upload File</Button>
+      </div>
+  
+      {/*files table data*/}
+      {data?<SimpleTable data={data} />:'....preloading'}
+      {/*end of files  table data*/}
+      
+      {/*upload File Modal*/}
+      <div>
+        <ModalCustom isOpen={false} opts={{
+          shownOn:'CLICK_ON_CREATE_COST_MODEL',
+          hideOn:'CLICK_ON_CANCEL_COST_MODEL' 
+        }}>
+          <Typography component="h2" variant="h2" gutterBottom>
+                  are you sure U want to delete {this.state.costPotToDelete}
+          </Typography>
+          <Button variant="contained" color="primary" onClick={()=>{events.emit('CLICK_ON_CANCEL_COST_MODEL');}}>Cancel Modal</Button>
+          <Button variant="contained" color="primary" onClick={()=>{events.emit('CLICK_ON_CANCEL_COST_MODEL');}}>Cancel Modal</Button>
+        </ModalCustom>
+      </div>
+      {/*end of upload File Modal*/}
+    </div>
+  );
 }
    
     
 }
-export default CostModel;
+export default withRouter(CostModel);
---
+++ src/services/filetypes-mock.js
@@ -0,0 +1,77 @@
+'use strict';
+import _ from '../utils/misc';
+
+const fileTypesMockService =  () => {
+  const responseMiddleware = (res) => {
+    return {
+      data: res.data || [],
+    };
+  };
+
+  const ServiceTypesMap = {
+    '27': 'General Ledger',
+    '36': 'Services',
+    '42': 'Storage',
+    '37': 'Organisation Capabilities',
+    '38': 'IT Functional Breakdown',
+    '39': 'Generic',
+    '31': 'Data Centre',
+  };
+
+  const items = [
+    {
+      id: 42,
+      resourceTypeId: 20,
+      code: 'SERVICES',
+      name: 'Services Mock1',
+      description: 'Contains a list of services',
+      type: 'SYSTEM',
+      multipleActive: false,
+      createdBy: 'rc1@amalytics.co',
+      creationDate: '2018-06-22T14:59:23.191Z',
+      tenantId: '1',
+    },
+    {
+      id: 43,
+      resourceTypeId: 20,
+      code: 'RATIO',
+      name: 'Ratio Mock2',
+      description: 'Contains a list of services',
+      type: 'SYSTEM',
+      multipleActive: false,
+      createdBy: 'rc1@amalytics.co',
+      creationDate: '2018-06-22T14:59:23.191Z',
+      tenantId: '1',
+    },
+  ];
+
+  const prepare = (items, resourceId) => {
+    return items.map((e) => {
+      // e.resourceTypeId = resourceId;
+      // e.name = ServiceTypesMap[resourceId.toString()];
+      // e.code = ServiceTypesMap[resourceId.toString()].toUpperCase();
+
+      return e;
+    });
+  };
+
+  const getAll = (resourceId) => {
+    return new Promise((resolve) => {
+      console.log('resourceId', resourceId);
+      const set = prepare(items, resourceId);
+      console.log('set', set);
+      setTimeout(() => {
+        resolve({
+          data: prepare(items, resourceId),
+          totalItemCount: _.length(items),
+        });
+      }, 0);
+    }).then(responseMiddleware);
+  };
+
+  return {
+    getAll,
+  };
+};
+
+export default fileTypesMockService();
