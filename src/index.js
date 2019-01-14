8d4e0cef780dc839305d35f39718740f7a93d056
GILGRZ <gradek64@yahoo.com>
2019-01-14 11:18:11 +0000

    heroku test major change

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ThemedApp from './appWithTheme';
+//import ThemedApp from './appWithTheme';
+
+const ThemedApp = () => <h1>this i a test heroku</h1>
+
 
 ReactDOM.render(<ThemedApp />, document.getElementById('app'));
