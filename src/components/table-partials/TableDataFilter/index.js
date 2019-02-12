e70450214b24832cf60f189577cc212a50dd2748
GILGRZ <gradek64@yahoo.com>
2019-02-12 23:17:35 +0000

    filter data continues

--- src/components/table-partials/TableDataFilter/index.js
+++ src/components/table-partials/TableDataFilter/index.js
@@ -8,8 +8,6 @@ import ListItemText from '@material-ui/core/ListItemText';
 
 import { withStyles } from '@material-ui/core/styles';
 
-
-
 const styles = {
   root: {
     color: green[600],
@@ -20,43 +18,62 @@ const styles = {
   checked: {},
 };
 
-
-
 class TableDataFilter extends React.Component {
 
    inputs = Array.from(Array(this.props.items.length), () => true)
    state = {
-     checkedAll:true,
-     checked:this.inputs,
+     checkedSelectAll:this.props.checkedSelectAll,
+     checked:this.props.filterDataSelected,
      atLeastOneChecked:true,
-   };
+   }
+
+   componentDidUpdate(prevProps) {
+     if (this.props.filterDataSelected !== prevProps.filterDataSelected) {
+       this.setState({
+         checked:this.props.filterDataSelected,
+         checkedSelectAll:this.props.checkedSelectAll
+       });
+     }
+   }
 
   handleChange = index => event => {
 
+      
     if(event.currentTarget.id==='selectAll'){
-      const { checkedAll } = this.state;
-      const allChecked = Array.from(Array(this.props.items.length), () => true);
+
+      const { checkedSelectAll } = this.state;
+      const allChecked = this.state.checked.fill(true);
+
+      console.log('checkedSelectAll', checkedSelectAll);
       this.setState(()=>{
         return {
-          checkedAll:this.state.checkedAll===false?!checkedAll:this.state.checkedAll,
+          checkedSelectAll:checkedSelectAll===false?true:checkedSelectAll,
           checked:[...allChecked]
         };
+      },()=>{
+      //update records in ReportsTableDataStructure and selectAll button;
+        this.props.updateRecord(this.state.checked, this.state.checkedSelectAll);
       });
+ 
     }else {
       
+      //make sure that U will update if at least one is selected 
       if(this.state.atLeastOneChecked || this.state.checked[index]!==true){
         this.state.checked[index]= !this.state.checked[index];
       }
       this.setState(()=>{
         return {
-          checkedAll:false,
+          checkedSelectAll:false,
           checked:[...this.state.checked]
         };
       },()=>{
         const howManyUnselected = this.state.checked.filter((input)=>input===false);
+        //check how many is unselected to make sure at least one is selected
         howManyUnselected.length===this.state.checked.length-1?
           this.setState({atLeastOneChecked:false}):
           this.setState({atLeastOneChecked:true});
+        //update records in ReportsTableDataStructure and selectAll button;
+        this.props.updateRecord(this.state.checked, this.state.checkedSelectAll);
       });
     }
 
@@ -72,9 +89,9 @@ class TableDataFilter extends React.Component {
       <List dense className={classes.root}>
         <ListItem role={undefined} dense button onClick={this.handleChange(null)} id='selectAll'>
           <Checkbox
-            checked={this.state.checkedAll}
+            checked={this.state.checkedSelectAll}
             tabIndex={-1}
-            value="checkedAll"
+            value="this.state.checkedSelectAll"
             disableRipple
             classes={{
               root: classes.root,
--- src/components/table-partials/dataStructure/ReportsTableDataStructure/index.js
+++ src/components/table-partials/dataStructure/ReportsTableDataStructure/index.js
@@ -14,76 +14,136 @@ import { withStyles } from '@material-ui/core/styles';
 //scss
 import './reportsTableDataStructure.scss';
 
-const TableDataReports = (props) => { 
-  const { classes ,pageUpdate, rowsPerPageUpdate, hasHeader=true ,hasFooter=true} = props;
-  const columns = ['label','percentage','value'];
-  {/* <Paper style={{overflowX:'scroll',overflowY:'initial'}}>*/}
-  return (
-    <Paper>
-      <Table className='table-fixed' style={{ minWidth:'860px'}}>
-        
-        {hasHeader?<TableHead >
-          <TableRow>
-            {hasHeader && columns.map((label,i)=>
-              <TableCell component="th" 
-                scope="row" 
-                key={`labelKey${i}`} 
-                dropdownmenuanchor='yes'>
-                {label}
-                <DropDownMenu 
-                  key={i}
-                  onMouseEnter={false} 
-                  collapsebleAccordion={false}
-                  multipleOpen={false}
-                  goesAwayOnContentClick={false}
-                  animation={true} >
-                  <div className='verticalAlignment' >
-                    {label}
-                  </div>
-                  <div>
-                    <Paper>
-                      <TableDataFilter items={props.data} accessor={label} />
-                      {/*<ul>
-                        { props.data.map((item,i)=>
-                          <li component="th" scope="row" key={`dropdown${i}`}>
-                            {item[label]}
-                          </li>:null)
-                        }
-                      </ul> */} 
-                    </Paper>            
-                  </div>
-                </DropDownMenu>
-              </TableCell>)}
-           
-          </TableRow>
-        </TableHead>:null}
-        <TableBody >
-          {props.data?props.data.map((item,index) => {
-            return (
-              <TableRow key={`table-reports${index}`} >
-                { columns.map((column,i)=>Object.keys(item).includes(column)?
-                  <TableCell component="th" scope="row" key={`tableKey${i}`}>
-                    {item[column]}
-                  </TableCell>:null)
-                }
-              </TableRow>
-            );
-          }):null}
-        </TableBody>
-        {  hasFooter? (
-          <TableFooter>
-            <TableRow>
-              {columns.map((label,i)=>
-                <TableCell component="th" scope="row" key={`labelKey${i}`}>
-                  {i===0?'total':null}
-                  {(i+1)===columns.length?'53534545645':null}
-                </TableCell>)}
-            </TableRow>
-          </TableFooter>) : null
-        }
-      </Table>
-    </Paper>
-  );
-};
+class TableDataReports extends React.Component { 
 
+  /*
+    *@U need to keep track what data has been filter for all 
+    *@filter applied by <TableDataFilter> component, all selected initially;
+  */
+  state = {
+    data:this.props.data,
+    filterDataSelected:Array.from(Array(this.props.data.length), () => true),
+    checkedSelectAll:true,
+  }
+
+  componentDidMount = () =>{
+    console.log('initially this.props.data', this.props.data);
+    //add addtional property:selected to this.state.data 
+    const dataSelection = this.state.data.map((data,i)=>{
+      data.selected = this.state.filterDataSelected[i];
+      return data;
+    });
+    //update state with selected;
+    this.setState(()=>{
+      return{
+        data:[...dataSelection]
+      };
+    });
+  }
+
+  updateDataSelected = (updateArray, selectAllButton) => {
+    //update selected in data;
+    const dataSelection2 = this.state.data
+      .map((data,i)=>{
+        data.selected = updateArray[i];
+        return data;
+      })
+      .filter(({selected})=>selected);
+    
+    this.setState(()=>{
+      return {
+        checkedSelectAll:selectAllButton,
+        filterDataSelected:[...updateArray],
+        data:[...dataSelection2]
+      };
+    },()=>{
+
+      console.log(':::this.state.data ::: ', this.state.data);
+      //filter data based on select inputs;
+      //this.state.data.filter(({selected})=>selected);
+    });
+  }
+
+  render(){
+
+    const {  hasHeader=true ,hasFooter=true } = this.props;
+
+    //to do 
+    const cloneData = Object.assign({}, this.props.data);
+
+    console.log('this.props.data', this.props.data);
+    console.log('cloneData', cloneData);
+    const { data, filterDataSelected, checkedSelectAll } = this.state;
+    const columns = ['label','percentage','value'];
+
+
+
+    {/* <Paper style={{overflowX:'scroll',overflowY:'initial'}}>*/}
+    return (
+      <Paper>
+        <Table className='table-fixed' style={{ minWidth:'860px'}}>
+        
+          {hasHeader?<TableHead >
+            <TableRow>
+              {hasHeader && columns.map((label,i)=>
+                <TableCell component="th" 
+                  scope="row" 
+                  key={`labelKey${i}`} 
+                  dropdownmenuanchor='yes'>
+                  {label}
+                  <DropDownMenu 
+                    key={i}
+                    onMouseEnter={false} 
+                    collapsebleAccordion={false}
+                    multipleOpen={false}
+                    goesAwayOnContentClick={false}
+                    animation={true} >
+                    <div className='verticalAlignment' >
+                      {label}
+                    </div>
+                    <div>
+                      <Paper>
+                        <TableDataFilter 
+                          items={data} 
+                          filterDataSelected={filterDataSelected}
+                          checkedSelectAll={checkedSelectAll}
+                          updateRecord={this.updateDataSelected}
+                          accessor={label} 
+                        />
+                      </Paper>            
+                    </div>
+                  </DropDownMenu>
+                </TableCell>)}
+           
+            </TableRow>
+          </TableHead>:null}
+          <TableBody >
+            {data?data.map((item,index) => {
+              return (
+                <TableRow key={`table-reports${index}`} >
+                  { columns.map((column,i)=>Object.keys(item).includes(column)?
+                    <TableCell component="th" scope="row" key={`tableKey${i}`}>
+                      {item[column]}
+                    </TableCell>:null)
+                  }
+                </TableRow>
+              );
+            }):null}
+          </TableBody>
+          {  hasFooter? (
+            <TableFooter>
+              <TableRow>
+                {columns.map((label,i)=>
+                  <TableCell component="th" scope="row" key={`labelKey${i}`}>
+                    {i===0?'total':null}
+                    {(i+1)===columns.length?'53534545645':null}
+                  </TableCell>)}
+              </TableRow>
+            </TableFooter>) : null
+          }
+        </Table>
+      </Paper>
+    );
+  }
+}
 export default TableDataReports;
