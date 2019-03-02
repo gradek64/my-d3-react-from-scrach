5dbf2ac554f9ef56e62315bf9c47a617ea067099
GILGRZ <gradek64@yahoo.com>
2019-03-02 11:03:05 +0000

    costpots airbnb done

--- src/pages/Admin/Costpots/costPotBox/index.js
+++ src/pages/Admin/Costpots/costPotBox/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import SvgIcon from '@material-ui/core/SvgIcon';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
+import PropTypes from 'prop-types';
+
+/* import SvgIcon from '@material-ui/core/SvgIcon';
 import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button'; */
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import Icon from '@material-ui/core/Icon';
 import './costPotBox.scss';
 
@@ -22,7 +24,7 @@ const styles = {
     mmaxWidth: 300,
   },
   extraLarge: {
-    fontSize:'100px'
+    fontSize: '100px',
   },
   bullet: {
     display: 'inline-block',
@@ -37,41 +39,57 @@ const styles = {
   },
 };
 
-const CostPtoBox = (props) => {
-  const { classes, name, heroIcon, hideDelete,hideAndroid, actionIcons, iconColor } = props; 
-  //deconstract for 'assignment_turned_in' icon
+const CostPotBox = (props) => {
+  const {
+    classes, name, heroIcon, hideDelete, hideAndroid, actionIcons, iconColor,
+  } = props;
+  // deconstract for 'assignment_turned_in' icon
   const { icon, linkParams } = actionIcons.assignment_turned_in;
-  return (<Card className={classes.card}>
-    <CardContent className={'customStyleBox'}>
-      <Typography className={classes.title} color="textSecondary"  variant="h1" component="h1" gutterBottom>
-        {name}
-      </Typography>
-      <Icon className={`${classes.icon} ${classes.extraLarge}`} color={iconColor?iconColor:'secondary'}>  
-        {heroIcon}                
-      </Icon>
-      <div className={'icons'}>
-        {
-          !hideDelete? <Icon className={classes.icon} color={iconColor?iconColor:'primary'} onClick={actionIcons.delete.action}>  
-            {actionIcons.delete.icon}                
-          </Icon>:null
-        }
-        {
-          !hideAndroid? <NavLink 
-            to={`/admin/cost-models/${linkParams.costModelId}/costpots/${linkParams.costPotId}/dataset-filters/`}>
-            <Icon className={classes.icon} color={iconColor?iconColor:'primary'}>  
-              {actionIcons.android.icon}                
-            </Icon></NavLink>:null
-        }
-        <NavLink to={`/admin/cost-models/${linkParams.costModelId}/costpots/${linkParams.costPotId}/file-management/`}>
-          <Icon className={classes.icon} color={iconColor?iconColor:'primary'}>  
-            {icon}                
-          </Icon></NavLink>
-      </div>
-    </CardContent>
-   
-   
-  </Card>
+  return (
+    <Card className={classes.card}>
+      <CardContent className="customStyleBox">
+        <Typography className={classes.title} color="textSecondary" variant="h1" component="h1" gutterBottom>
+          {name}
+        </Typography>
+        <Icon className={`${classes.icon} ${classes.extraLarge}`} color={iconColor || 'secondary'}>
+          {heroIcon}
+        </Icon>
+        <div className="icons">
+          {
+            !hideDelete ?
+              <Icon className={classes.icon} color={iconColor || 'primary'} onClick={actionIcons.delete.action}>
+                {actionIcons.delete.icon}
+              </Icon> : null
+          }
+          {
+            !hideAndroid ?
+              <NavLink
+                to={`/admin/cost-models/${linkParams.costModelId}/costpots/${linkParams.costPotId}/dataset-filters/`}
+              >
+                <Icon className={classes.icon} color={iconColor || 'primary'}>
+                  {actionIcons.android.icon}
+                </Icon>
+              </NavLink> : null
+          }
+          <NavLink to={`/admin/cost-models/${linkParams.costModelId}/costpots/${linkParams.costPotId}/file-management/`}>
+            <Icon className={classes.icon} color={iconColor || 'primary'}>
+              {icon}
+            </Icon>
+          </NavLink>
+        </div>
+      </CardContent>
+    </Card>
   );
 };
 
-export default withStyles(styles)(CostPtoBox);
+CostPotBox.propTypes = {
+  selectDropdownDataFixed: PropTypes.instanceOf(Array),
+  onSubmit: PropTypes.instanceOf(Function),
+};
+
+CostPotBox.defaultProps = {
+  selectDropdownDataFixed: [],
+  onSubmit: () => {},
+};
+
+export default withStyles(styles)(CostPotBox);
--- src/pages/Admin/Costpots/index.js
+++ src/pages/Admin/Costpots/index.js
@@ -1,202 +1,194 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
-import resourceTypeMockService from '../../../services/resource-types-mock';
 import AppBar from '@material-ui/core/AppBar';
-import levelsMockService from '../../../services/levels-mock';
-import costpotsMockService from '../../../services/costpots-mock';
-import CostPotBox from './costPotBox';
-import { NavLink } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-//import ModalCustom from '../../../components/modal';
+import PropTypes from 'prop-types';
+import { NavLink, withRouter } from 'react-router-dom';
+
+// import ModalCustom from '../../../components/modal';
+// import resourceTypeMockService from '../../../services/resource-types-mock';
+
+import CostPotBox from './costPotBox';
+import levelsMockService from '../../../services/levels-mock';
+import costpotsMockService from '../../../services/costpots-mock';
 import ModalCustom from '../../../customized-vendors/modalVendor';
 
 import _ from '../../../utils/misc';
 import events from '../../../utils/events';
-
 import './scss/costpots.scss';
 
-const buildResourceTypeItem = (e) => {
-  return {
-    label: e.label,
-    value: {type: e.label, id: e.value.id},
-  };
-};
-const buildInfrastructureLevelItem = (e) => {
-  return {
-    label: e.name,
-    value: {
-      id: e.id,
-      classification: 'INFRASTRUCTURE',
-      parentId: null,
-    },
-  };
-};
+/* const buildResourceTypeItem = e => ({
+  label: e.label,
+  value: { type: e.label, id: e.value.id },
+});
+const buildInfrastructureLevelItem = e => ({
+  label: e.name,
+  value: {
+    id: e.id,
+    classification: 'INFRASTRUCTURE',
+    parentId: null,
+  },
+}); */
 
 class CostPots extends React.Component {
-/*
-  *@state could be empty object
-  *@react will understand any changes to its properties as undefined
-*/
-state = {
-/*  allLevels:null,*/
-}
+  constructor(props) {
+    super(props);
+    // levels
+    this.getLevels()
+      .then(res => res.data)
+      .then(levels => levels.sort(_.compareFactory('order', false, true)))
+      .then((allLevels) => {
+        // costPots;
+        this.getCostPots()
+          .then(res => res.data)
+          .then((allCostPosts) => {
+            this.setState({
+              allLevels,
+              allCostPosts,
+            });
+          });
+      });
+  }
+
+  state = {
+    allLevels: null,
+    allCostPosts: null,
+  }
+
+  componentDidMount() {
+    const { costModelId } = this.props;
+
+    this.breadcrumbsLinks = [
+      {
+        href: '/admin/cost-models',
+        label: 'Cost Models',
+      },
+      {
+        href: `/admin/cost-models/${costModelId}/costpots`,
+        label: 'Model Config',
+      },
+    ];
+  }
+
+getLevels = () => levelsMockService.getAll()
+getCostPots = () => costpotsMockService.getAll(this.props.costModelId)
+
 breadcrumbsLinks = [];
 
-async componentDidMount(){
-
-  const { costModelId } = this.props;
-
-  this.breadcrumbsLinks = [
-    {
-      href: '/admin/cost-models',
-      label: 'Cost Models',
-    },
-    {
-      href: '/admin/cost-models/' + costModelId + '/costpots',
-      label: 'Model Config',
-    }
-  ];
-
-  const allResourceTypes = await resourceTypeMockService.getAll();
-  const resourceTypeItems = allResourceTypes.data.map(buildResourceTypeItem);
-  const levelsRespond = await levelsMockService.getAll();
-  const allLevels = levelsRespond.data.sort(
-    _.compareFactory('order', false, true)
-  );
-  const infrastructureLevels = levelsRespond.data
-    .filter((e) => e.domainId === 3)
-    .map(buildInfrastructureLevelItem);
-
-
-  //get costposts
-  const allCostPosts = await costpotsMockService.getAll(costModelId);
-  //update state;
-  this.setState(() => ({ 
-    allLevels,
-    allCostPosts :allCostPosts.data,
-  }));
-
-}
-
-deleteCostPot(costPotName){
-  this.setState({costPotToDelete:costPotName});
+deleteCostPot(costPotName) {
+  this.setState({ costPotToDelete: costPotName });
   events.emit('OPEN_MODAL');
 }
 
-render(){
-  const {allLevels, allCostPosts} = this.state;
+render() {
+  const { allLevels, allCostPosts } = this.state;
 
-  //list of hero icons here temporary before U make some configuration file
+  // list of hero icons here temporary before U make some configuration file
   const heroIcons = {
-    36:'code',
-    37:'backup',
-    38:'backup',
-    39:'backup',
-    40:'backup',
-    41:'backup',
-    42:'backup',
-    43:'backup',
-    44:'backup',
-    45:'backup',
-    46:'build',
-    47:'backup',
-    48:'card_travel',
-   
+    36: 'code',
+    37: 'backup',
+    38: 'backup',
+    39: 'backup',
+    40: 'backup',
+    41: 'backup',
+    42: 'backup',
+    43: 'backup',
+    44: 'backup',
+    45: 'backup',
+    46: 'build',
+    47: 'backup',
+    48: 'card_travel',
+
   };
 
-  if(this.state.allLevels){
+  if (allLevels) {
     return (
-      <div className='costpots'>
-        <AppBar position="static" color="default" className='breadcrumbsLinks'>
+      <div className="costpots">
+        <AppBar position="static" color="default" className="breadcrumbsLinks">
 
           <div >
             <Typography component="h6" variant="subtitle1" gutterBottom>
               {
-                /*avoid using <a> tags cause they reload entire page causing lag use NavLink instead*/
+                /* avoid using <a> tags cause they reload
+                entire page causing lag use NavLink instead */
 
-                this.breadcrumbsLinks.map(({href, label},id)=>
+                this.breadcrumbsLinks.map(({ href, label }, id) =>
                   (
                     <NavLink to={href} key={`bread${id}`}>
-                      {label} 
-                      {(id!==this.breadcrumbsLinks.length-1) ?' > ':null}
+                      {label}
+                      {(id !== this.breadcrumbsLinks.length - 1) ? ' > ' : null}
                     </NavLink>
-                  )
-                )
+                  ))
               }
             </Typography>
           </div>
         </AppBar>
         <div className="section graph">
-         
+
           {
-            allLevels.map((levelProps,id)=>{
-              return (
-                <div className="row" key={`level${id}`}>
-                  <div className={'levelCostPots'} key={`costpot${id}`}>
-                    { 
-                      allCostPosts.map((attrs,id)=> {
-                        if(levelProps.id === attrs.levelId){
-                          const {configurationId:costModelId, id:costPotId} = attrs;
-                          console.log(attrs);
-                          return (
-                            <CostPotBox 
-                              key={`costpotBox${id}`}
-                              name={attrs.name}
-                              heroIcon={heroIcons[36+id]}
-                              actionIcons={{
-                                delete:{icon:'delete',action:()=>{ this.deleteCostPot(attrs.name);},linkParams:null},
-                                android:{
-                                  icon:'androidIcon',
-                                  action:null, 
-                                  linkParams:{costModelId,costPotId}
-                                },
-                                assignment_turned_in:{
-                                  icon:'assignment_turned_inIcon',
-                                  action:null,
-                                  linkParams:{costModelId,costPotId}
-                                }
-                              }}
-                              hideDelete = {levelProps.domainId !== 3}
-                              hideAndroid = {levelProps.domainId !== 3}
-                            />
-                          );
-                        } else {
-                          return null;
-                        }
-                      
-                      }) 
-                    }  
-                  </div>  
-                  <h6 className={`level-name domain-${levelProps.domainId}`} >{levelProps.name}</h6>            
+            allLevels.map((levelProps, id) => (
+              <div className="row" key={`level${id}`}>
+                <div className="levelCostPots" key={`costpot${id}`}>
+                  {allCostPosts ?
+                    allCostPosts.map((attrs, index) => {
+                      if (levelProps.id === attrs.levelId) {
+                        const { configurationId: costModelId, id: costPotId } = attrs;
+                        return (
+                          <CostPotBox
+                            key={`costpotBox${index}`}
+                            name={attrs.name}
+                            heroIcon={heroIcons[36 + id]}
+                            actionIcons={{
+                              delete: { icon: 'delete', action: () => { this.deleteCostPot(attrs.name); }, linkParams: null },
+                              android: {
+                                icon: 'androidIcon',
+                                action: null,
+                                linkParams: { costModelId, costPotId },
+                              },
+                              assignment_turned_in: {
+                                icon: 'assignment_turned_inIcon',
+                                action: null,
+                                linkParams: { costModelId, costPotId },
+                              },
+                            }}
+                            hideDelete={levelProps.domainId !== 3}
+                            hideAndroid={levelProps.domainId !== 3}
+                          />
+                        );
+                      }
+                      return null;
+                    })
+                    : null}
                 </div>
-              );
-            })
+                <h6 className={`level-name domain-${levelProps.domainId}`} >{levelProps.name}</h6>
+              </div>
+            ))
           }
-         
-         
+
+
         </div>
-        {/*deleteCostPot Modal*/}
+        {/* deleteCostPot Modal */}
         <div>
           <ModalCustom isOpen={false} >
-            <Typography component="h3" variant="h3" gutterBottom style={{textAlign:'center'}}>
+            <Typography component="h3" variant="h3" gutterBottom style={{ textAlign: 'center' }}>
                   are you sure U want to delete {this.state.costPotToDelete}?
             </Typography>
-            <Button variant="contained" color="primary" className='buttonConfirm' onClick={()=>{events.emit('CLOSE_MODAL');}}>Delete</Button>
-            <Button variant="contained" color="primary" className='buttonCancel' onClick={()=>{events.emit('CLOSE_MODAL');}}>Cancel</Button>
+            <Button variant="contained" color="primary" className="buttonConfirm" onClick={() => { events.emit('CLOSE_MODAL'); }}>Delete</Button>
+            <Button variant="contained" color="primary" className="buttonCancel" onClick={() => { events.emit('CLOSE_MODAL'); }}>Cancel</Button>
           </ModalCustom>
         </div>
       </div>
     );
-  } else {
-    return '....preloading';
   }
+  return '....preloading';
+}
+}
 
-}
-   
-    
-}
-//add router to this component for getting currenct route params;
-//it will be attached to the props as props.location for location;
+CostPots.propTypes = {
+  costModelId: PropTypes.string.isRequired,
+};
+
+// add router to this component for getting currenct route params;
+// it will be attached to the props as props.location for location;
 export default withRouter(CostPots);
 
